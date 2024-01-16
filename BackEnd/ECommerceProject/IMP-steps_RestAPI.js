5b0e785a55828f89000d67373b4186a4f9583acd
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2024-01-17 00:51:12 +0530

    added steps to create rest api project and best practices

---
+++ BackEnd/ECommerceProject/IMP-steps_RestAPI.js
@@ -0,0 +1,50 @@
+// ## API Development Guide
+
+// ### 1. Folder Structure
+//    - Explanation of the recommended folder structure for organizing your API project.
+
+// ### 2. Route Configuration
+//    - How to configure routes for your API.
+
+// ### 3. Creating APIs
+//    - Steps and best practices for creating APIs.
+
+// ### 4. Different API Methods
+//    - Overview of various HTTP methods (GET, POST, PUT, DELETE) and their use in APIs.
+
+// ### 5. Middlewares
+//    - Explanation of middleware usage in API development for tasks like authentication, logging, etc.
+
+// ### 6. Route Parameters
+//    - Understanding and utilizing route parameters in API routes.
+
+// ### 7. Query Parameters
+//    - How to handle and use query parameters in API requests.
+
+// ### 8. Testing API Using Postman
+//    - Guide on using Postman for testing and debugging APIs.
+
+// ### 9. Securing API Using JWT Token
+//    - Implementing JSON Web Token (JWT) for API security.
+
+// ### 10. Documenting API Using Swagger
+//    - Steps to document your API using Swagger for better understanding and collaboration.
+
+// ### 11. CORS (Cross-Origin Resource Sharing)
+//    - Explanation of CORS and how to handle it in your API.
+
+// ### 12. Logging
+//    - Best practices for logging in your API to track and analyze activities.
+
+// ### 13. Error Handling
+//    - Strategies for effective error handling in API development.
+
+
+// REST API Best Practices
+
+// 1. Using Correct Status Codes: Proper HTTP status codes for clear communication.
+// 2. Error Handling: Strategies for effective error handling in API development.
+// 3. Input Validation: Ensuring data integrity through validation of incoming requests.
+// 4. Using Correct Request Methods: Utilizing appropriate HTTP methods (GET, POST, PUT, DELETE).
+// 5. Secured APIs: Implementing security measures, such as authentication and authorization.
+// 6. Use Naming Conventions for URL: Consistent and meaningful URL naming conventions.
