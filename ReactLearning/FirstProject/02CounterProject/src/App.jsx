4c0c448a752c3c5e07bd94239f3d25ada597be4d
MSI\Rahul Pawar <pawar.rahul1002@gmail.com>
2024-04-14 17:33:21 +0530

    made code readable

--- ReactLearning/FirstProject/02CounterProject/src/App.jsx
+++ ReactLearning/FirstProject/02CounterProject/src/App.jsx
@@ -2,31 +2,61 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [counter, setCount] = useState(90)
-  // let counter = 15;
+  let [counter, setCount] = useState(90);
 
   const addValue = () => {
-
     counter++;
     setCount(counter);
   };
 
   const decreaseValue = () => {
-
     counter--;
     setCount(counter);
   };
 
+  // function manipulateValue(isAdd)
+  // {
+  //   if(isAdd)
+  //   {
+  //     addValue();
+  //   }
+  //   else{
+  //     decreaseValue();
+  //   }
+  // }
+
+  function manipulateValue(isAdd) {
+    if (isAdd) {
+      counter++;
+    } else {
+      counter--;
+    }
+    setCount(counter);
+  }
 
   return (
     <>
       <h1>Chai aur React</h1>
       <br></br>
-      <button onClick={addValue}>Add</button>
+      {/* <button onClick={addValue}>Add</button> */}
+      <button
+        onClick={() => {
+          manipulateValue(true);
+        }}
+      >
+        Add
+      </button>
       <br></br>
       <h1>Count : {counter}</h1>
       <br></br>
-      <button onClick={decreaseValue}>Remove</button>
+      {/* <button onClick={decreaseValue}>Remove</button> */}
+      <button
+        onClick={() => {
+          manipulateValue(false);
+        }}
+      >
+        Remove
+      </button>
       <br></br>
     </>
   );
